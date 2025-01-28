e5b557a5d7894906a7c50b5b1bbb2a1c376ed4d0
Alexand Shaymardanov <sovereing92@mail.ru>
2025-01-28 23:45:14 +0600

    Добавление кнопки перехода на вверх, плавно

--- src/modules/smoothScrolling.js
+++ src/modules/smoothScrolling.js
@@ -1,7 +1,8 @@
 const smoothScrolling = () => {
   const smoothLinks = document.querySelectorAll("a[href^='#']");
+
   for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener("click", function (e) {
+    smoothLink.addEventListener("click", (e) => {
       e.preventDefault();
       const id = smoothLink.getAttribute("href");
 
@@ -11,5 +12,34 @@ const smoothScrolling = () => {
       });
     });
   }
+
+  // Получаем кнопку по ID
+  const scrollTopBtn = document.querySelector(".up");
+  console.log(scrollTopBtn);
+
+  // Добавляем событие на прокрутку окна
+  window.onscroll = () => {
+    toggleScrollTopBtn();
+  };
+
+  // Функция для отображения или скрытия кнопки
+  const toggleScrollTopBtn = () => {
+    if (
+      document.body.scrollTop > 300 ||
+      document.documentElement.scrollTop > 300
+    ) {
+      scrollTopBtn.style.display = "block";
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+  };
+
+  // Добавляем обработчик события для плавной прокрутки наверх
+  scrollTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Плавная прокрутка
+    });
+  });
 };
 export default smoothScrolling;
